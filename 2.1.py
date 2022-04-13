5e979b7c859d0895fe515ffb3a8e67a8618af95b
[Murmiau] <[mirgorodscya@mail.ru]>
2022-04-13 19:51:47 +0300

    упражнение 1

---
+++ 2.1.py
@@ -0,0 +1,4 @@
+my_list = [18, 11.2, 'Hello', [1, 7.4, 'name', True], (False, 96, 'nonono', 3.8), {'c', 300, None}, {3: "Hi", 5: 346},
+           frozenset({17, 6.3, 'my_name'}),True, None, ZeroDivisionError]
+for el in my_list:
+    print(type(el))
