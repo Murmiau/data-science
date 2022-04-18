7f2162ac6184e93eb27f4a72f9402b287faadb67
[Murmiau] <[mirgorodscya@mail.ru]>
2022-04-18 23:48:28 +0300

    задание 7

---
+++ 3.7.py
@@ -0,0 +1,14 @@
+def int_func(text):
+    lat = "zxcvbnmasdfghjklqwertyuiop"
+    if text.islower():
+        return text.title() if not set(text).difference(lat) else print\
+            ("Попробуйте еще раз ввести фразу на латинице в нижнем регистре!")
+    else:
+        print("Введите, пожалуйста, фразу на латинице в нижнем регистре!")
+
+text = input("Введите, пожалуйста, фразу, целиком состоящую "
+                      "из букв в нижнем регистре на латинице: ").split()
+for i in text:
+    res = int_func(i)
+    if res:
+        print(res, end=' ')
