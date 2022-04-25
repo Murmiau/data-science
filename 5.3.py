ecf3a15f1149bcbd1678d9f5f2f329dd1bf58e67
Murmiau <102258204+Murmiau@users.noreply.github.com>
2022-04-26 02:33:42 +0300

    Update 5.3.py

--- 5.3.py
+++ 5.3.py
@@ -1,10 +1,10 @@
-with open("homework-5.3.txt", "r") as my_zp:
-    sal = []
-    poor = []
+with open("hw5.txt", "r", encoding="UTF-8") as my_zp:
+    num = []
+    fil = []
     my_list = my_zp.read().split("\n")
     for i in my_list:
         i = i.split()
         if int(i[1]) < 20000:
-           poor.append(i[0])
-        sal.append(i[1])
-print(f'Оклад меньше 20.000 {poor}, средний оклад {sum(map(int, sal)) / len(sal)}')
+           fil.append(i[0])
+        num.append(i[1])
+print(f"Оклад меньше 20000: {fil}, средний оклад у специалистов: {sum(map(int, num)) / len(num)}")
